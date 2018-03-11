6a1ff2a505666bf0e4b56740ee3d43869256bac7
Timothy Horton <zurafuse@gmail.com>
2018-03-10 21:53:19 -0600

    Add files via upload

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,34 +1,34 @@
 import site_generator
 import media
 
-wonder_woman = media.Movie("Wonder Woman", "Description", "https://upload.wikimedia.org/wikipedia/en/e/ed/Wonder_Woman_%282017_film%29.jpg",
+wonder_woman = media.Movie("Wonder Woman", "Description", "images/wonder_woman.jpg",
                            "https://www.youtube.com/watch?v=VSB4wGIdDwo", '<iframe width="854" height="480" src="https://www.youtube.com/embed/VSB4wGIdDwo" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
 
-guardians_of_galaxy = media.Movie("Guardians of the Galaxy", "Description", "https://upload.wikimedia.org/wikipedia/en/thumb/8/8f/GOTG-poster.jpg/220px-GOTG-poster.jpg",
+guardians_of_galaxy = media.Movie("Guardians of the Galaxy", "Description", "images/guardians_of_the_galaxy.jpg",
                                   "https://www.youtube.com/watch?v=d96cjJhvlMA", '<iframe width="854" height="480" src="https://www.youtube.com/embed/d96cjJhvlMA" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
 
-guardians_of_galaxy2 = media.Movie("Guardians of the Galaxy Vol 2", "Description", "https://upload.wikimedia.org/wikipedia/en/thumb/9/95/GotG_Vol2_poster.jpg/220px-GotG_Vol2_poster.jpg",
+guardians_of_galaxy2 = media.Movie("Guardians of the Galaxy Vol 2", "Description", "images/guardians_of_the_galaxy2.jpg",
                                    "https://www.youtube.com/watch?v=2cv2ueYnKjg", '<iframe width="854" height="480" src="https://www.youtube.com/embed/2cv2ueYnKjg" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
 
-spiderman = media.Movie("Spider-Man: Homecoming", "2017 American superhero film based on the Marvel Comics character Spider-Man", "https://upload.wikimedia.org/wikipedia/en/thumb/f/f9/Spider-Man_Homecoming_poster.jpg/220px-Spider-Man_Homecoming_poster.jpg",
+spiderman = media.Movie("Spider-Man: Homecoming", "2017 American superhero film based on the Marvel Comics character Spider-Man", "images/spider-man.jpg",
                         "https://www.youtube.com/watch?v=oNE0zRNyXuc", '<iframe width="640" height="360" src="https://www.youtube.com/embed/oNE0zRNyXuc" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
 
 man_of_steel = media.Movie("Man of Steel", "2013 superhero film featuring the DC Comics character Superman", "images/man_of_steel.jpg",
                         "https://www.youtube.com/watch?v=T6DJcgm3wNY", '<iframe width="640" height="360" src="https://www.youtube.com/embed/T6DJcgm3wNY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
 
-batman_vs_superman = media.Movie("Batman v Superman: Dawn of Justice", "2016 American superhero film featuring the DC Comics characters Batman and Superman", "https://upload.wikimedia.org/wikipedia/en/thumb/2/20/Batman_v_Superman_poster.jpg/220px-Batman_v_Superman_poster.jpg",
+batman_vs_superman = media.Movie("Batman v Superman: Dawn of Justice", "2016 American superhero film featuring the DC Comics characters Batman and Superman", "images/batman_superman.jpg",
                         "https://www.youtube.com/watch?v=fis-9Zqu2Ro", '<iframe width="854" height="480" src="https://www.youtube.com/embed/fis-9Zqu2Ro" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
 
-justice_league = media.Movie("Justice League", "2017 American superhero film based on the DC Comics superhero team", "https://upload.wikimedia.org/wikipedia/en/3/31/Justice_League_film_poster.jpg",
+justice_league = media.Movie("Justice League", "2017 American superhero film based on the DC Comics superhero team", "images/justice_league.jpg",
                         "https://www.youtube.com/watch?v=r9-DM9uBtVI", '<iframe width="640" height="360" src="https://www.youtube.com/embed/r9-DM9uBtVI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
 
-dbz_rezF = media.Movie("Dragon Ball Z: Resurrection F", "Description", "https://upload.wikimedia.org/wikipedia/en/thumb/6/6a/DBZ_THE_MOVIE_NO._15.png/220px-DBZ_THE_MOVIE_NO._15.png",
+dbz_rezF = media.Movie("Dragon Ball Z: Resurrection F", "Description", "images/dbz_resurrection_f.png",
                         "https://www.youtube.com/watch?v=WiONylGn8Xw", '<iframe width="854" height="480" src="https://www.youtube.com/embed/WiONylGn8Xw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')
 
 #incredibles = media.Movie("The Incredibles", "A Pixar super hero film.", "https://upload.wikimedia.org/wikipedia/en/thumb/e/ec/The_Incredibles.jpg/220px-The_Incredibles.jpg",
 #                        "https://www.youtube.com/watch?v=eZbzbC9285I", '<iframe width="640" height="360" src="https://www.youtube.com/embed/eZbzbC9285I" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')	
 
-incredibles2 = media.Movie("The Incredibles 2", "2nd installment of the Pixar super hero film.", "https://upload.wikimedia.org/wikipedia/en/2/27/The_Incredibles_2.jpg",
+incredibles2 = media.Movie("The Incredibles 2", "2nd installment of the Pixar super hero film.", "images/The_Incredibles_2.jpg",
                         "https://www.youtube.com/watch?v=2_VhcvodRBY", '<iframe width="640" height="360" src="https://www.youtube.com/embed/2_VhcvodRBY" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>')					
 						
 #toy_story = media.Movie("Toy Story", "A movie about a boy and his toys which come to life when he is not looking.",
